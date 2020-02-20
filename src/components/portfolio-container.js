42fc58e984b0dd38fde2595e9ae7d8de9897d1c4
austin smythe <aust.burner@gmail.com>
2020-02-20 16:29:12 -0700

    added portfolio-container page with h2 tag

---
+++ src/components/portfolio-container.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class portfolioContainer extends Component {
+    render() {
+        return (
+            <div className='portfolio-container'>
+                <h2> Austin Smythe portfolio container </h2>
+            </div>
+        );
+    }
+}
