1bd5af4bc0924762e3c3d7fe87a5c97092830ddb
austin smythe <aust.burner@gmail.com>
2020-02-21 10:08:48 -0700

    added another portfolio item

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
+import PortfolioItem from "./portfolio-items"
+
 export default class portfolioContainer extends Component {
     render() {
         return (
             <div className='portfolio-container'>
-                <h2> Austin Smythe portfolio container </h2>
+                <h2> Portfolio Items go here </h2>
+                <PortfolioItem />
             </div>
         );
     }
