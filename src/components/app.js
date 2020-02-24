d790591b6a9b64445bb5e74f1d8586aa7ca4f871
austin smythe <aust.burner@gmail.com>
2020-02-24 15:58:50 -0700

    added a router for the home and about

--- src/components/app.js
+++ src/components/app.js
@@ -1,20 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import moment from "moment";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import PortfolioContainer from "./portfolio/portfolio-container"
-import NavigationContainer from "./navigation/navigation-container"
+import PortfolioContainer from "./portfolio/portfolio-container";
+import NavigationContainer from "./navigation/navigation-container";
+import Home from "./pages/home";
+import About from "./pages/about";
 
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
+      <div className="app">
         <NavigationContainer />
+        <Router>
+          <div>
+          
+
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about-me" component={About} />
+            </Switch>
+          </div>
+        </Router>
+
         <h1>Austin Smythe Portfolio</h1>
-        <div>
-          {moment().format('MMMM Do YYYY, h:mm:ss a')}
-          <PortfolioContainer />
+        <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
+        <PortfolioContainer />
         </div>
-      </div>
+      
     );
   }
 }
