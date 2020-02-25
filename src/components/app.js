aac6116b9e9705bdb922712d95befdf621e56792
austin smythe <aust.burner@gmail.com>
2020-02-25 10:11:46 -0700

    added dynamic slug for link items

--- src/components/app.js
+++ src/components/app.js
@@ -2,36 +2,33 @@ import React, { Component } from "react";
 import moment from "moment";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import PortfolioContainer from "./portfolio/portfolio-container";
 import NavigationContainer from "./navigation/navigation-container";
 import Home from "./pages/home";
 import About from "./pages/about";
 import Contact from "./pages/contact";
-import Blog from "./pages/blog"
+import Blog from "./pages/blog";
+import PortfolioDetail from "./portfolio/portfolio-detail";
 
 export default class App extends Component {
   render() {
     return (
       <div className="app">
-        <NavigationContainer />
         <Router>
           <div>
-          
+            <h1>Jordan Hudgens Portfolio</h1>
+            <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
+            <NavigationContainer />
 
             <Switch>
               <Route exact path="/" component={Home} />
               <Route path="/about-me" component={About} />
-              <Route path="/Contact" component={Contact} />
-              <Route path="/Blog" component={Blog} />
+              <Route path="/contact" component={Contact} />
+              <Route path="/blog" component={Blog} />
+              <Route path="/portfolio/:slug" component={PortfolioDetail} />
             </Switch>
           </div>
         </Router>
-
-        <h1>Austin Smythe Portfolio</h1>
-        <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
-        <PortfolioContainer />
-        </div>
-      
+      </div>
     );
   }
 }
--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,59 +1,66 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import PortfolioItem from "./portfolio-items"
+import PortfolioItem from "./portfolio-item";
 
-export default class portfolioContainer extends Component {
-    constructor() {
-        super();
+export default class PortfolioContainer extends Component {
+  constructor() {
+    super();
 
-        this.state = {
-            pageTitle: "Welcome to my Portfolio",
-            isLoading: false,
-            data: [
-                {title: "Quip", category: "eCommerce" },
-                {title: "Eventbrite", category: "scheduling" },
-                {title: "Ministry Safe", category: "Enterprise" },
-                {title: "SwingAway", category: "eCommerce" }
+    this.state = {
+      pageTitle: "Welcome to my portfolio",
+      isLoading: false,
+      data: [
+        { title: "Quip", category: "eCommerce", slug: "quip" },
+        { title: "Eventbrite", category: "Scheduling", slug: "eventbrite" },
+        {
+          title: "Ministry Safe",
+          category: "Enterprise",
+          slug: "ministry-safe"
+        },
+        { title: "SwingAway", category: "eCommerce", slug: "swingaway" }
+      ]
+    };
 
-            ]
-        };
-        this.handleFilter = this.handleFilter.bind(this);
+    this.handleFilter = this.handleFilter.bind(this);
+  }
+
+  handleFilter(filter) {
+    this.setState({
+      data: this.state.data.filter(item => {
+        return item.category === filter;
+      })
+    });
+  }
+
+  portfolioItems() {
+    return this.state.data.map(item => {
+      return (
+        <PortfolioItem title={item.title} url={"google.com"} slug={item.slug} />
+      );
+    });
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return <div>Loading...</div>;
     }
 
-    handleFilter(filter) {
-        this.setState({
-            data: this.state.data.filter(item => {
-                return item.category === filter
-            })
-        })
-    }
+    return (
+      <div>
+        <h2>{this.state.pageTitle}</h2>
 
-    portfolioItems() {
-        
+        <button onClick={() => this.handleFilter("eCommerce")}>
+          eCommerce
+        </button>
+        <button onClick={() => this.handleFilter("Scheduling")}>
+          Scheduling
+        </button>
+        <button onClick={() => this.handleFilter("Enterprise")}>
+          Enterprise
+        </button>
 
-       return this.state.data.map(item => {
-           return <PortfolioItem title={item.title} url="google.com"/>;
-       })
-    }
-
-
-    render() {
-        if (this.state.isLoading) {
-            return <div> Loading...</div>;
-        }
-
-        return (
-            <div className='portfolio-container'>
-                <h2>{this.state.pageTitle}</h2>
-
-                <button onClick={() => this.handleFilter('eCommerce')}>eCommerce</button>
-                <button onClick={() => this.handleFilter('Scheduling')}>Scheduling</button>
-                <button onClick={() => this.handleFilter('Enterprise')}>Enterprise</button>
-                
-                {this.portfolioItems()}
-
-                
-            </div>
-        );
-    }
+        {this.portfolioItems()}
+      </div>
+    );
+  }
 }
