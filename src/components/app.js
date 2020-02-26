0603e0411e3dcd952c190044bf9c0f528372db2f
austin smythe <aust.burner@gmail.com>
2020-02-26 13:32:29 -0700

    removed moment and title from app.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import moment from "moment";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import NavigationContainer from "./navigation/navigation-container";
@@ -23,8 +22,7 @@ export default class App extends Component {
       <div className="app">
         <Router>
           <div>
-            <h1>Austin Smythe Portfolio</h1>
-            <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
+            
             <NavigationContainer />
 
             <Switch>
