9aafeb513472a050463c2bd44933a078a0fd74e7
austin smythe <aust.burner@gmail.com>
2020-02-28 13:53:10 -0700

    added login features and auth certification

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,46 +1,42 @@
-import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom'
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
 
-export default class navigationContainer extends Component {
-    constructor() {
-        super();
-    }
+export default class NavigationComponent extends Component {
+  constructor() {
+    super();
+  }
 
-    adminLinks() {
+  render() {
+    return (
+      <div className="nav-wrapper">
+        <div className="left-side">
+          <div className="nav-link-wrapper">
+            <NavLink exact to="/" activeClassName="nav-link-active">
+              Home
+            </NavLink>
+          </div>
 
-    }
+          <div className="nav-link-wrapper">
+            <NavLink to="/about-me" activeClassName="nav-link-active">
+              About
+            </NavLink>
+          </div>
 
-        render() {
-            return (
-                <div className="nav-wrapper">
-                    <div className="left-side">
-                        <div className="nav-link-wrapper">
-                            <NavLink exact to="/">
-                                Home
-                            </NavLink>
-                        </div>
+          <div className="nav-link-wrapper">
+            <NavLink to="/contact" activeClassName="nav-link-active">
+              Contact
+            </NavLink>
+          </div>
 
-                        <div className="nav-link-wrapper">
-                            <NavLink exact to="/about-me" activeClassName="nav-link-active">
-                                About
-                            </NavLink>
-                        </div>
+          <div className="nav-link-wrapper">
+            <NavLink to="/blog" activeClassName="nav-link-active">
+              Blog
+            </NavLink>
+          </div>
+        </div>
 
-                        <div className="nav-link-wrapper">
-                            <NavLink exact to="/contact" activeClassName="nav-link-active">
-                                Contact
-                            </NavLink>
-                        </div>
-
-                        <div className="nav-link-wrapper">
-                            <NavLink exact to="/Blog" activeClassName="nav-link-active">
-                                Blog
-                            </NavLink>
-                        </div>
-
-                    </div>
-                    <div className="right-side">Austin Smythe</div>
-                </div>
-            )
-        }
+        <div className="right-side">Austin Smythe</div>
+      </div>
+    );
+  }
 }
