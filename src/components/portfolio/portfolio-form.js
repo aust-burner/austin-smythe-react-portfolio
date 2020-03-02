72be9d17d044e67a9eb4e48c94883267611a7429
austin smythe <aust.burner@gmail.com>
2020-03-02 15:59:46 -0700

    uploading thumbnail images to the api in React

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -26,6 +26,12 @@ export default class PortfolioForm extends Component {
     this.djsConfig = this.djsConfig.bind(this);
   }
 
+  handleThumbDrop() {
+      return {
+        addedfile: file => this.setState({ thumb_image: file })
+      }
+  }
+
   componentConfig() {
     return {
       iconFiletypes: [".jpg", ".png"],
@@ -49,6 +55,9 @@ export default class PortfolioForm extends Component {
     formData.append("portfolio_item[url]", this.state.url);
     formData.append("portfolio_item[category]", this.state.category);
     formData.append("portfolio_item[position]", this.state.position);
+    if (this.state.thumb_image) {
+        formData.append("portfolio_item[thumb_image]", this.state.thumb_image);
+      }
 
     return formData;
   }
@@ -134,6 +143,7 @@ export default class PortfolioForm extends Component {
             <DropzoneComponent
               config={this.componentConfig()}
               djsConfig={this.djsConfig()}
+              eventHandlers={this.handleThumbDrop()}
             />
           </div>
 
