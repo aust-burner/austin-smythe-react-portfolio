fb65085d22dfa58cdc83fc4e3b6f9830fed2a190
austin smythe <aust.burner@gmail.com>
2020-03-10 15:50:42 -0600

    added a ton of stuff and forgot to push. Most current is communicating with Blog Update API Endpoint

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -1,6 +1,9 @@
-  
 import React, { Component } from "react";
 import axios from "axios";
+import ReactHtmlParser from "react-html-parser";
+
+import BlogForm from "../blog/blog-form";
+import BlogFeaturedImage from "../blog/blog-featured-image";
 
 export default class BlogDetail extends Component {
   constructor(props) {
@@ -8,22 +11,50 @@ export default class BlogDetail extends Component {
 
     this.state = {
       currentId: this.props.match.params.slug,
-      blogItem: {}
+      blogItem: {},
+      editMode: false
     };
+
+    this.handleEditClick = this.handleEditClick.bind(this);
+    this.handleFeaturedImageDelete = this.handleFeaturedImageDelete.bind(this);
+    this.handleUpdateFormSubmission = this.handleUpdateFormSubmission.bind(
+      this
+    );
+  }
+
+  handleUpdateFormSubmission(blog) {
+    this.setState({
+      blogItem: blog,
+      editMode: false
+    });
+  }
+
+  handleFeaturedImageDelete() {
+    this.setState({
+      blogItem: {
+        featured_image_url: ""
+      }
+    });
+  }
+
+  handleEditClick() {
+    console.log("handle edit clicked");
+    this.setState({ editMode: true });
   }
 
   getBlogItem() {
     axios
       .get(
-        `https://austinsmythe.devcamp.space/portfolio/portfolio_blogs/${this.state
-          .currentId}`
+        `https://austinsmythe.devcamp.space/portfolio/portfolio_blogs/${
+          this.state.currentId
+        }`
       )
       .then(response => {
         this.setState({
           blogItem: response.data.portfolio_blog
         });
       })
-      .catch(error => { 
+      .catch(error => {
         console.log("getBlogItem error", error);
       });
   }
@@ -39,18 +70,30 @@ export default class BlogDetail extends Component {
       featured_image_url,
       blog_status
     } = this.state.blogItem;
-        
-    return (
-        <div className="blog-container">
+
+    const contentManager = () => {
+      if (this.state.editMode) {
+        return (
+          <BlogForm
+            handleFeaturedImageDelete={this.handleFeaturedImageDelete}
+            handleUpdateFormSubmission={this.handleUpdateFormSubmission}
+            editMode={this.state.editMode}
+            blog={this.state.blogItem}
+          />
+        );
+      } else {
+        return (
           <div className="content-container">
-            <h1>{title}</h1>
-            <div className="featured-image-wrapper">
-                <img src={featured_image_url} />
-            </div>
-            <div className="content>
-            ">{content}</div>
+            <h1 onClick={this.handleEditClick}>{title}</h1>
+
+            <BlogFeaturedImage img={featured_image_url} />
+
+            <div className="content">{ReactHtmlParser(content)}</div>
           </div>
-        </div>
-      );
+        );
+      }
+    };
+
+    return <div className="blog-container">{contentManager()}</div>;
   }
 }
